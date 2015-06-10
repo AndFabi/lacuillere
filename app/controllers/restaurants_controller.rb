ce67637c7ff13ded0d26347f0b606b995cec24ee
AndFabi <fabiandreacola@gmail.com>
2015-06-10 17:11:27 +0200

    reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,33 +4,33 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params [:id])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to @restaurant
+    restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(restaurant)
   end
 
   def edit
     @restaurant = Restaurant.find_by(params[:id])
+    @restaurant.update()
 
   end
 
   def delete
-
+    @restaurant = Restaurant.find_by(params[:id])
+    @restaurant.destroy
 
   end
 
   def restaurant_params
-    params
-      .require(:restaurants)
-      .permit(:name, :address, :phone_number, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+
   end
 
 
